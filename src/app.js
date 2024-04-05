e6c4ebc81fc8d5298256651f3242342c1dd149fa
Miguel Angel <160284274+Mitronios@users.noreply.github.com>
2024-04-05 16:02:58 +0000

    First commit of my excuse generator

--- src/app.js
+++ src/app.js
@@ -2,10 +2,39 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = generateExcuse();
+  });
+};
+
+let generateExcuse = () => {
+  let who = ["My cat ", "My computer ", "My code ", "My girlfriend "];
+  let action = ["ate ", "scratched ", "burned ", "throw "];
+  let what = ["my homework ", "my phone ", "my car ", "my clothes "];
+  let when = [
+    " before the class",
+    " when I was sleeping",
+    " while I was at the supermarket",
+    " during my break",
+    " while I was taking a shower"
+  ];
+
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+
+  return (
+    who[whoIndex] +
+    "" +
+    action[actionIndex] +
+    "" +
+    what[whatIndex] +
+    "" +
+    when[whenIndex]
+  );
 };
